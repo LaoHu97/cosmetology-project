8839f35c3b5b01b9978f03c4970c963541ec49e3
LaoHu97 <962271461@qq.com>
2018-02-01 09:17:42 +0800

    2018-2-1

--- src/views/Payment.vue
+++ src/views/Payment.vue
@@ -14,7 +14,7 @@
 
 <script>
 import wx from 'weixin-js-sdk'
-import { prepayInfo, updateInviter, receiveCardAfter, addCardConfig } from '../api.js'
+import { prepayInfo, updateInviter } from '../api.js'
 import { XButton, Icon, XHeader, XInput, Group, Radio, Box } from 'vux'
 export default {
   components: {
@@ -35,18 +35,46 @@ export default {
   created () {
     console.log(this.$route.params.amount)
     console.log(this.$route.params.pkg_name)
-    this.wxConfig()
+    // this.wxConfig()
   },
   methods: {
-    wxConfig () {
-      let payData = JSON.parse(sessionStorage.getItem('payData'))
+    // wxConfig () {
+    //   let payData = JSON.parse(sessionStorage.getItem('payData'))
+    //   let para = {
+    //     mid: String(payData.mid),
+    //     url: window.location.href.split('#')[0]
+    //   }
+    //   addCardConfig(para).then((res) => {
+    //     wx.config({
+    //       debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+    //       appId: res.data.configinfo.appId, // 必填，公众号的唯一标识
+    //       timestamp: res.data.configinfo.timestamp, // 必填，生成签名的时间戳
+    //       nonceStr: res.data.configinfo.noncestr, // 必填，生成签名的随机串
+    //       signature: res.data.configinfo.configSign, // 必填，签名，见附录1
+    //       jsApiList: ['addCard'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+    //     })
+    //     wx.ready(function () {
+    //       // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
+    //     })
+    //     wx.error(function (res) {
+    //       // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
+    //     })
+    //   })
+    // },
+    wxAddCard (data) {
       let para = {
-        mid: String(payData.mid),
+        mid: String(data.mid),
+        purchaseId: String(data.purchaseId),
+        ive: data.ive,
+        ivr: data.ivr,
+        sc: String(data.source),
         url: window.location.href.split('#')[0]
       }
-      addCardConfig(para).then((res) => {
+      para.ivr = para.ivr == null ? para.ivr : String(para.ivr)
+      para.ive = para.ive == null ? para.ive : String(para.ive)
+      updateInviter(para).then((res) => {
         wx.config({
-          debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+          debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
           appId: res.data.configinfo.appId, // 必填，公众号的唯一标识
           timestamp: res.data.configinfo.timestamp, // 必填，生成签名的时间戳
           nonceStr: res.data.configinfo.noncestr, // 必填，生成签名的随机串
@@ -54,55 +82,29 @@ export default {
           jsApiList: ['addCard'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
         })
         wx.ready(function () {
+          let cardExtdata = {
+            signature: res.data.cardMap.signature,
+            nonce_str: res.data.cardMap.nonce_str,
+            timestamp: res.data.cardMap.timestamp,
+            outer_str: res.data.cardMap.outer_str
+          }
+          let purchaseId = res.data.cardMap.purchaseId
+          let _this = this
+          wx.addCard({
+            cardList: [{
+              cardId: res.data.cardMap.card_id,
+              cardExt: JSON.stringify(cardExtdata)
+            }], // 需要添加的卡券列表
+            success: function (res) {
+              _this.$router.push({
+                path: '/complete'
+              })
+            }
+          })
           // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
         })
         wx.error(function (res) {
           // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
-        })
-      })
-    },
-    wxAddCard (data) {
-      let para = {
-        mid: String(data.mid),
-        purchaseId: String(data.purchaseId),
-        ive: data.ive,
-        ivr: data.ivr,
-        sc: String(data.source)
-      }
-      para.ivr = para.ivr == null ? para.ivr : String(para.ivr)
-      para.ive = para.ive == null ? para.ive : String(para.ive)
-      updateInviter(para).then((res) => {
-        let cardExtdata = {
-          signature: res.data.signature,
-          nonce_str: res.data.nonce_str,
-          timestamp: res.data.timestamp,
-          outer_str: res.data.outer_str
-        }
-        let purchaseId = res.data.purchaseId
-        let _this = this
-        wx.addCard({
-          cardList: [{
-            cardId: res.data.card_id,
-            cardExt: JSON.stringify(cardExtdata)
-          }], // 需要添加的卡券列表
-          success: function (res) {
-            let payData = JSON.parse(sessionStorage.getItem('payData'))
-            let para = {
-              mid: String(payData.mid),
-              ive: String(payData.ive),
-              purchaseId: purchaseId,
-              cardCode: res.cardList[0].code,
-              cardid: res.cardList[0].cardId,
-              isSuccess: res.cardList[0].isSuccess,
-              res: res
-            }
-            receiveCardAfter(para).then((res) => {
-              alert('领卡通知成功')
-              _this.$router.push({
-                path: '/complete'
-              })
-            })
-          }
         })
       })
     },
